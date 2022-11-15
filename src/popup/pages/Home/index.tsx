5fbda3e45e54174573f59271f706766bcd0850ce
arthurXu <a619928793@gmail.com>
2022-11-16 00:18:46 +0800

    feat: home page ui

--- src/popup/pages/Home/index.tsx
+++ src/popup/pages/Home/index.tsx
@@ -3,16 +3,25 @@ import './index.scss'
 import EigenIcon from '@assets/images/logo.png'
 
 function Home() {
+
+
+
+
+ const googleLogin = async () => {
+    
+ }
+
+
     return (
         <div className="container">
             <div className="icon">
-                111
                 <img src={EigenIcon} alt=""/>
                 <p className="eigenName">EigenSafe</p>
                 <p className="eigenDesc">Simple, Sage and Reliable</p>
             </div>
-            <div className="login">
-                Google Login
+            <div className="login" onClick={googleLogin}>
+              <i className="login-icon logo-google"></i>
+              <span>Google Account</span>
             </div>
         </div>
     )
